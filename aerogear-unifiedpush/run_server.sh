40b6f72ea5de1156fb93add9df6f2c4c71db31b5
MuXian Wu <muxian.wu@gogame.net>
2016-06-10 16:26:20 +0800

    1. Wildly separated to 9.0.0 and 10.0.0. 2. Aerogear push server updated to 1.2.0.alpha

--- aerogear-unifiedpush/run_server.sh
+++ aerogear-unifiedpush/run_server.sh
@@ -1,1 +1,1 @@
-docker run -d --env-file env_vars -p 8080:8080 --name unifiedpush wumuxian/aerogear-unifiedpush:1.1.0
+docker run -d --env-file env_vars -p 8080:8080 --name unifiedpush wumuxian/aerogear-unifiedpush:1.2.0
